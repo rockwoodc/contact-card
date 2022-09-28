a1b47a034a2c7a0b4de27254204c275e65e57797
rockwoodc <rockwoodce@gmail.com>
2022-09-28 16:58:38 -0400

    bundle css

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -5,6 +5,8 @@ import "./submit";
 import Logo from '../images/logo.png'
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+// Import CSS files
+import "../css/index.css";
 
 window.addEventListener('load', function () {
     document.getElementById('logo').src = Logo;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,20 +1,33 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/js/index.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    mode: "development",
-    //rule allows us to handle images
-    module: {
-        rules: [
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  //rule allows us to handle images
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
-        ]
-      }
+        },
+      },
+    ],
+  },
 };
-
