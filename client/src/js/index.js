231ceb7d62e34342c9b173b902af060b98abc24d
rockwoodc <rockwoodce@gmail.com>
2022-09-28 16:34:42 -0400

    bundled images

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,3 +1,13 @@
 //this file serves as an entry point-- this will pull the logic from both files below
 import "./form";
 import "./submit";
+//allows us to use and import images
+import Logo from '../images/logo.png'
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+
+window.addEventListener('load', function () {
+    document.getElementById('logo').src = Logo;
+    document.getElementById('bearThumbnail').src = Bear;
+    document.getElementById('dogThumbnail').src = Dog;
+  });
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,18 @@ const path = require("path");
 module.exports = {
     entry: "./src/js/index.js",
     output: {
-        filename: "main.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
     mode: "development",
+    //rule allows us to handle images
+    module: {
+        rules: [
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ]
+      }
 };
+
